6d4f85d6a146b620f59c0fe23e8c39e90e68837b
Sagar Pandey <sagar@Sagars-MacBook-Air.local>
2026-04-15 02:42:01 +0530

    smart scheduling

--- src/main/java/com/sagarpandey/activity_tracker/Service/Interface/ActivityServiceInterface.java
+++ src/main/java/com/sagarpandey/activity_tracker/Service/Interface/ActivityServiceInterface.java
@@ -1,4 +1,4 @@
-git package com.sagarpandey.activity_tracker.Service.Interface;
+package com.sagarpandey.activity_tracker.Service.Interface;
 
 import com.sagarpandey.activity_tracker.dtos.ActivityResponse;
 import com.sagarpandey.activity_tracker.models.Activity;
@@ -8,9 +8,14 @@ import java.util.List;
 
 public interface ActivityServiceInterface {
     Activity create(HashMap<String, String> ActivityInfo);
+
     Activity read(Long id);
+
     List<ActivityResponse> readAll();
+
     List<ActivityResponse> readAll(String userId);
+
     void update(HashMap<String, String> ActivityInfo);
+
     void delete(Long id);
 }
